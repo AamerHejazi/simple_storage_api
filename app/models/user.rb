d495ade3ed2a852de4aa1c34a6defb61b33a179f
Amer Hijazi <e-a.hijazi@lean.sa>
2024-04-10 07:46:44 +0300

    add validation for email not dublicate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
+    
+    validates :email, presence: true, uniqueness: true
+
     has_many :auth_tokens, dependent: :destroy
     has_many :blobs, dependent: :destroy
 end
