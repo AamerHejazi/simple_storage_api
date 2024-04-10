12e89fdc5fd09799168c8871e3fec0c5d0691303
Amer Hijazi <e-a.hijazi@lean.sa>
2024-04-10 07:39:05 +0300

    changes realted to auth bearer token

--- app/models/auth_token.rb
+++ app/models/auth_token.rb
@@ -1,3 +1,19 @@
 class AuthToken < ApplicationRecord
   belongs_to :user
+
+   # Callback to generate a unique token before creating a new AuthToken record
+   before_create :generate_unique_secure_token
+
+   # Callback to set the expiration time for the token
+   before_create :set_expiration_time
+ 
+   private
+ 
+   def generate_unique_secure_token
+     self.token = SecureRandom.hex(10) # Generates a random token
+   end
+ 
+   def set_expiration_time
+     self.expires_at ||= 24.hours.from_now # Sets the token to expire in 24 hours
+   end
 end
